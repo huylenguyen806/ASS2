a5d1bcfb0ad45e1c9f617d6d398bf6ff3efe7544
huylenguyen806 <nlhuy1998@gmail.com>
2017-05-13 01:36:09 +0700

    Add files via upload
    Clean files :)

---
+++ manageaccount.cpp
@@ -0,0 +1,40 @@
+#include "manageaccount.h"
+#include "ui_manageaccount.h"
+
+ManageAccount::ManageAccount(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::ManageAccount)
+{
+    ui->setupUi(this);
+    setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    setWindowModality(Qt::ApplicationModal);
+}
+
+ManageAccount::~ManageAccount()
+{
+    delete ui;
+}
+
+void ManageAccount::on_Cancel_reset_pass_clicked()
+{
+    ui->reset_pass_account_name->clear();
+    ui->reset_pass->clear();
+    this->close();
+}
+
+void ManageAccount::on_Cancel_edit_account_clicked()
+{
+    ui->edit_account_name->clear();
+    ui->edit_Role_account->clear();
+    ui->edit_username_account->clear();
+    this->close();
+}
+
+void ManageAccount::on_Cancel_add_account_clicked()
+{
+    ui->add_account_name->clear();
+    ui->add_password->clear();
+    ui->add_role->clear();
+    ui->add_user_name->clear();
+    this->close();
+}
---
+++ manageaccount.h
@@ -0,0 +1,30 @@
+#ifndef MANAGEACCOUNT_H
+#define MANAGEACCOUNT_H
+
+#include <QWidget>
+
+namespace Ui {
+class ManageAccount;
+}
+
+class ManageAccount : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit ManageAccount(QWidget *parent = 0);
+    ~ManageAccount();
+
+
+public slots:
+    void on_Cancel_reset_pass_clicked();
+
+    void on_Cancel_edit_account_clicked();
+
+    void on_Cancel_add_account_clicked();
+
+private:
+    Ui::ManageAccount *ui;
+};
+
+#endif // MANAGEACCOUNT_H
