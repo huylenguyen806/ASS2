711546fdb4a7ba2d61aea1e8394566444c8bff01
huylenguyen806 <nlhuy1998@gmail.com>
2017-05-14 02:29:58 +0700

    continue3

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowIcon(QIcon(":/images/Libpro_icon.ico"));
     show_viewer();
+    createBookWidget();
 }
 
 MainWindow::~MainWindow()
@@ -14,10 +16,78 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::login()
+{
+
+}
+
+void MainWindow::createBookWidget()
+{
+    //vertical layout in book_scoll_area
+    QVBoxLayout *vlayout = new QVBoxLayout(ui->book_scoll_area);
+    for(int i = 0; i < 10; ++i){
+    //create widget contains books' info and button
+    QWidget *newbook = new QWidget();
+    newbook->setMinimumHeight(100);
+    newbook->setMaximumHeight(100);
+    //create horizontal layout
+    QHBoxLayout *newlayout = new QHBoxLayout(newbook);
+    //create label contains books' info
+    QLabel *newlabel = new QLabel();
+    newlabel->setText("Newbook");
+    //create button
+    QPushButton *newbutton = new QPushButton();
+    newbutton->setText("Put in basket");
+    //create line
+    QFrame *line = new QFrame();
+    line->setFrameShape(QFrame::HLine);
+    line->setFrameShadow(QFrame::Sunken);
+    //add widgets
+    newlayout->addWidget(newlabel,5,0);
+    newlayout->addWidget(newbutton,1,0);
+    vlayout->addWidget(newbook,0,0);
+    vlayout->addWidget(line,0,0);
+    }
+    //create spacer
+    QSpacerItem *newspacer = new QSpacerItem(20,20,QSizePolicy::Expanding,QSizePolicy::Expanding);
+    vlayout->addSpacerItem(newspacer);
+}
+
+void MainWindow::createMessageBox(QString type, QString text)
+{
+    QMessageBox *message = new QMessageBox();
+    message->setText(text);
+    if(type == "Error")
+    {
+        message->setWindowTitle("Error");
+        message->setWindowIcon(QIcon(":/images/error.png"));
+    }
+    else if (type == "Warning")
+    {
+        message->setWindowTitle("Warning");
+        message->setWindowIcon(QIcon(":/images/warning.png"));
+    }
+    else if(type == "OK")
+    {
+        message->setWindowTitle("Success");
+        message->setWindowIcon(QIcon(":/images/OK.png"));
+    }
+    message->show();
+}
+
+void MainWindow::clear_all_lineEdit()
+{
+    foreach (QLineEdit *sth, findChildren<QLineEdit* >())
+    {
+        sth->clear();
+    }
+}
+
 void MainWindow::show_viewer()
 {
     ui->role_select->clear();
     ui->role_select->addItem("Viewer");
+    ui->mainStack->setCurrentWidget(ui->rolePage);
     ui->main_ui_2->setCurrentWidget(ui->not_login);
     ui->main_ui->setCurrentWidget(ui->find_books);
 }
@@ -26,22 +96,17 @@ void MainWindow::on_basket_clicked()
 {
     if(ui->login_button->text() == "Login")
     {
-        QMessageBox *alert = new QMessageBox();
-        alert->setWindowTitle("Error");
-        alert->setText("Please login to continue.");
-        alert->show();
+        createMessageBox("Error", "Please login to continue.");
     }
     else{
-        QMessageBox *noti = new QMessageBox();
-        noti->setWindowTitle("Success");
-        noti->setText("The book is now in your basket.");
-        noti->show();
+        createMessageBox("OK", "The book is now in your basket.");
     }
 }
 
 void MainWindow::on_edit_book_manage_button_clicked()
 {
     BookManagement *edit = new BookManagement();
+    edit->showeditbook();
     edit->show();
 }
 
@@ -63,10 +128,11 @@ void MainWindow::on_info_button_clicked()
 {
     if(ui->login_button->text() == "Login")
     {
-        QMessageBox *alert = new QMessageBox();
-        alert->setWindowTitle("Error");
-        alert->setText("Please login to see your info.");
-        alert->show();
+        createMessageBox("Error", "Please login to see your info.");
+    }
+    else if(ui->login_button->text() == "Logout")
+    {
+        ui->main_ui->setCurrentWidget(ui->info);
     }
 }
 
@@ -78,4 +144,66 @@ void MainWindow::on_login_button_clicked()
 void MainWindow::on_close_login_form_button_clicked()
 {
     ui->mainStack->setCurrentWidget(ui->rolePage);
+    clear_all_lineEdit();
+}
+
+void MainWindow::on_reader_find_book_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->find_books);
+    clear_all_lineEdit();
+}
+
+void MainWindow::on_goto_basket_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->basket_stack);
+}
+
+void MainWindow::on_reader_change_pass_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->change_password);
+    clear_all_lineEdit();
+}
+
+void MainWindow::on_manager_change_pass_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->change_password);
+    clear_all_lineEdit();
+}
+
+void MainWindow::on_librarian_change_pass_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->change_password);
+    clear_all_lineEdit();
+}
+
+void MainWindow::on_reader_noti_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->reader_noti);
+}
+
+void MainWindow::on_manage_account_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->account_manage);
+}
+
+void MainWindow::on_manage_info_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->user_info_manage);
+}
+
+void MainWindow::on_manage_book_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->books_info);
+}
+
+void MainWindow::on_manage_demand_button_clicked()
+{
+    ui->main_ui->setCurrentWidget(ui->reader_demand);
+}
+
+void MainWindow::on_reset_pass_button_clicked()
+{
+    ManageAccount *reset = new ManageAccount();
+    reset->showresetpass();
+    reset->show();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -6,6 +6,11 @@
 #include "bookmanagement.h"
 #include "manageaccount.h"
 #include <QPicture>
+#include <QWidget>
+#include <QLabel>
+#include <QBoxLayout>
+#include <QSpacerItem>
+#include <QFrame>
 namespace Ui {
 class MainWindow;
 }
@@ -34,6 +39,36 @@ private slots:
     void on_login_button_clicked();
 
     void on_close_login_form_button_clicked();
+
+    void clear_all_lineEdit();
+
+    void login();
+
+    void createMessageBox(QString type, QString text);
+
+    void on_reader_find_book_button_clicked();
+
+    void on_goto_basket_button_clicked();
+
+    void on_reader_change_pass_button_clicked();
+
+    void on_manager_change_pass_button_clicked();
+
+    void on_librarian_change_pass_button_clicked();
+
+    void on_reader_noti_button_clicked();
+
+    void on_manage_account_button_clicked();
+
+    void on_manage_info_button_clicked();
+
+    void on_manage_book_button_clicked();
+
+    void on_manage_demand_button_clicked();
+
+    void on_reset_pass_button_clicked();
+
+    void createBookWidget();
 
 private:
     Ui::MainWindow *ui;
